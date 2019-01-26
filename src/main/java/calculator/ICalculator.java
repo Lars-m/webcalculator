1dcb98be9773dfd45d98112b57a8fd4799fe962f
lars-m <lam@cphbusiness.dk>
2019-01-26 22:11:20 +0100

    minor

--- src/main/java/calculator/ICalculator.java
+++ src/main/java/calculator/ICalculator.java
@@ -12,7 +12,7 @@ interface ICalculator {
    * Subtracts two numbers
    * @param n1 The first number
    * @param n2 Then second number
-   * @returns n1 - n2
+   * @return n1 - n2
    */
   int sub(int n1, int n2);
   int mul(int n1, int n2);
