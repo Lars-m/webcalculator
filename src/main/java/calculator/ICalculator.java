0d06237d1f5e8b18d10964237c8fcf5b4742db4b
lars-m <lam@cphbusiness.dk>
2019-01-26 18:55:52 +0100

    minor

--- src/main/java/calculator/ICalculator.java
+++ src/main/java/calculator/ICalculator.java
@@ -8,6 +8,12 @@ interface ICalculator {
    * @return the sum of n1+n2
    */
   int add(int n1, int n2);
+  /**
+   * Subtracts two numbers
+   * @param n1 The first number
+   * @param n2 Then second number
+   * @returns n1 - n2
+   */
   int sub(int n1, int n2);
   int mul(int n1, int n2);
   int div(int n1, int n2) throws ArithmeticException;
