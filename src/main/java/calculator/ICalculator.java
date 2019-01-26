67acb47137a0d1ca4444cde79d72f2a74a778527
lars-m <lam@cphbusiness.dk>
2019-01-26 15:45:49 +0100

    with site

--- src/main/java/calculator/ICalculator.java
+++ src/main/java/calculator/ICalculator.java
@@ -1,6 +1,12 @@
 package calculator;
 
 interface ICalculator {
+  /**
+   * Adds two numbers
+   * @param n1 The first number
+   * @param n2 Then second number
+   * @return the sum of n1+n2
+   */
   int add(int n1, int n2);
   int sub(int n1, int n2);
   int mul(int n1, int n2);
