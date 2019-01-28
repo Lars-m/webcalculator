7a59ff6fdef2fb37763018e400cc5fb79e9de83e
Lars Mortensen (LAM - Adjunkt - Cphbusiness) <lam@cphbusiness.dk>
2019-01-28 09:43:59 +0100

    minor

--- src/test/java/integrationtests/WebCalculatorTest.java
+++ src/test/java/integrationtests/WebCalculatorTest.java
@@ -31,8 +31,8 @@ public class WebCalculatorTest {
   
   @Test
   public void testWebMul() throws IOException{
-    String res = client.makeHttpRequest("?operation=mul&n1=6&n2=12");
-    assertEquals("Result of: 6*12= 72", res);
+    String res = client.makeHttpRequest("?operation=mul&n1=6&n2=6");
+    assertEquals("Result of: 6*6= 32", res);
   }
   
   
